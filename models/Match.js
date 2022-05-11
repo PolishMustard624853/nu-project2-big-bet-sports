9f523c96851ed30b274b2e42dcddb57fd24fabd2
Reniel Christianson <waldochristianson@gmail.com>
2022-05-11 14:23:47 -0500

    made delete button functionable

--- models/Match.js
+++ models/Match.js
@@ -1,40 +1,40 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Match extends Model {}
+class Match extends Model { }
 
 Match.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        home_team: {
+            type: DataTypes.STRING(30),
+            allowNull: false,
+        },
+        away_team: {
+            type: DataTypes.STRING(30),
+            allowNull: false,
+        },
+        league_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            references: {
+                model: 'leagues',
+                key: 'id',
+            },
+        },
     },
-    home_team: {
-      type: DataTypes.STRING(30),
-      allowNull: false,
-    },
-    away_team: {
-      type: DataTypes.STRING(30),
-      allowNull: false,
-    },
-    league_id: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      references: {
-        model: 'leagues',
-        key: 'id',
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'matches',
-  }
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'matches',
+    }
 );
 
 module.exports = Match;
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,8 +1,17 @@
+// const updateButtons = document.querySelectorAll(".update")
+// for (let i = 0; i < delButtons.length; i++) {
+  //   delButtons[i].addEventListener("click", delButtonHandler)
+  // }
+  // for (let i = 0; i < delButtons.length; i++) {
+    //   updateButtons[i].addEventListener("click", UpdateHandler)
+    // }
+    // const delButtons =  document.querySelectorAll(".delete")
 const delButtonHandler = async (event) => {
+
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/projects/${id}`, {
+    const response = await fetch(`/api/bet/${id}`, {
       method: 'DELETE',
     });
 
