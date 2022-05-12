40801958bd0408c2fed370cba6700d705e7526af
Alec Dziwanowski <adziwanowski@gmail.com>
2022-05-12 15:29:10 -0400

    commented glob in index.js

--- models/index.js
+++ models/index.js
@@ -2,10 +2,15 @@ const glob = require('glob');
 
 const model = {};
 
+// glob.sync changes glob function from asynchronous to synchronous
+// find files in models folder that end in .js except index.js
 const files = glob.sync('./models/*.js', { ignore: "./models/index.js" });
 files.forEach(file => {
+  // grab file name for each file grabbed using regex
   let key = file.match(/[A-Z][a-zA-Z]+/);
+  // create variable named the same name as file name and require that file
   this[key[0]] = require(`./${key[0]}`);
+  // set variable above as property of object
   model[key[0]] = this[key[0]];
 });
 
